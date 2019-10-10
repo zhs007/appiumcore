eae36183642f28e9e7899a1ec3f80be78b0a7974
zhs007 <sssxueren@gmail.com>
2019-10-10 16:21:10 +0800

    upd

--- src/test001.js
+++ src/test001.js
@@ -1,23 +1,27 @@
-const wd = require("wd");
-const { configure } = require("./log");
+const wd = require('wd');
+const {configure} = require('./log');
 
 const serverConfig = {
-  host: "localhost",
-  port: 4723
+  host: 'localhost',
+  port: 4723,
 };
 
+/**
+   * start
+   * @param {object} cfg - {host, port}
+   */
 function start(cfg) {
-  let driver = wd.promiseChainRemote(cfg);
+  const driver = wd.promiseChainRemote(cfg);
   configure(driver);
 
-  var desired = {
-    platformName: "Android",
-    appPackage: "com.taobao.trip",
-    appActivity: ".home.HomeActivity",
-    deviceName: "OnePlus3T",
-    platformVersion: "9.0",
-    udid: "c104c463",
-    noReset: true
+  const desired = {
+    platformName: 'Android',
+    appPackage: 'com.taobao.trip',
+    appActivity: '.home.HomeActivity',
+    deviceName: 'OnePlus3T',
+    platformVersion: '9.0',
+    udid: 'c104c463',
+    noReset: true,
   };
 
   driver.init(desired).setImplicitWaitTimeout(8000);
